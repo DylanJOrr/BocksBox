03594906dc2202818f4072bb3754e8c24e7b35dd
DylanJOrr <contact@dylanorr.com>
2023-07-22 16:47:25 -0400

    Added Home Page Images

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,52 @@
+import { Button, Container, Image, Row, Col } from 'react-bootstrap';
+
 const HomePage = () => {
 	return (
-		<div className="HomePage">
-			<section className="catch vh-100">
-				<h2>Catch</h2>
-				<p></p>
-				<i>1</i>
+    <>
+        <Container>
+            <Container>
+                <Row>
+                    <Image src="src/img/sushi.png" fluid />
+                    <Col>
+                        <h2>Catch Your Craving</h2>
+                        <Button variant="primary">Get Started</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+		<div className="HomePage d-flex flex-column justify-content-between">
+			<section className="discover">
+                <div className="d-flex flex-row justify-content-center">
+                    <div className="d-flex flex-column justify-content-center align-items-center">
+                        <h2>Find Your Food</h2>
+                        <Button variant="primary">Discover</Button>
+                    </div>
+                    <Image className="" src="src/img/fooditems.png" />
+                </div>
+                <i>1</i>
 			</section>
-			<section className="discover vh-100">
-				<h2>Discover</h2>
-				<p></p>
-				<i>2</i>
+			<section className="build">
+                <div className="d-flex flex-row justify-content-evenly m-5">
+                    <Image src="src/img/foodbox.png" fluid />
+                    <div className="d-flex flex-column justify-content-center align-items-center m-5">
+                        <h2>Build Your Box</h2>
+                        <Button variant="primary">Build</Button>
+                    </div>
+                </div>
+                <i>1</i>
 			</section>
-			<section className="build vh-100">
-				<h2>Build</h2>
-				<p></p>
-				<i>3</i>
-			</section>
-			<section className="order vh-100">
-				<h2>Order</h2>
-				<p></p>
-				<i>4</i>
+			<section className="order">
+                <div className="d-flex flex-row justify-content-evenly m-5">
+                    <div className="d-flex flex-column justify-content-center align-items-center m-5">
+                        <h2>Order Your Way</h2>
+                        <Button variant="primary">Get Started</Button>
+                    </div>
+                    <Image src="src/img/order.png" fluid />
+                </div>
+                <i>1</i>
 			</section>
 		</div>
+        </>
 	);
 };
 
